17e71788a0b910271fa1596360ea2ebb3002dae1
GDVillalba <guilledvil@gmail.com>
2023-04-02 20:38:20 -0300

    Carga Inicial De ejercicio del triangulo

---
+++ EjercicioTriangulo/src/triangulo/Triangulo.java
@@ -0,0 +1,72 @@
+package triangulo;
+
+public class Triangulo {
+	
+	private int lado1;
+	private int lado2;
+	private int lado3;
+	
+	public Triangulo(){}
+	
+	public Triangulo( int lado1, int lado2, int lado3) 
+	{
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+		this.lado3 = lado3;
+	}
+	
+	public boolean esTriangulo() 
+	{
+		if( this.lado1 + this.lado2 > this.lado3)
+		{
+			return true;
+		}
+		if( this.lado2 + this.lado3 > this.lado1)
+		{
+			return true;
+		}
+		if( this.lado3 + this.lado1 > this.lado2)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public String getTipo() 
+	{
+	
+		System.out.println( this.lado1 + " - " + this.lado2 + " - " + this.lado3);
+		
+		if( (this.lado1 == this.lado2) && (this.lado1 == this.lado3) && (this.lado2 == this.lado3))
+		{
+			return "equilatero";
+		}
+		
+		if( (this.lado1 == this.lado2 && this.lado1 != this.lado3) || 
+			(this.lado2 == this.lado3 && this.lado2 != this.lado1) ||
+			(this.lado3 == this.lado1 && this.lado3 != this.lado2) )
+		{
+			return "isoceles";
+		}
+		else
+		{
+			return "rectangulo";
+		}
+
+	}
+	
+	public int getLado(int lado)
+	{
+		if( lado == 1 )
+			return this.lado1;
+		
+		if( lado == 2 )
+			return this.lado2;
+		
+		if( lado == 3 )
+			return this.lado3;
+		
+		return 0;
+	}
+	
+}
---
+++ EjercicioTriangulo/src/triangulo/main.java
@@ -0,0 +1,40 @@
+package triangulo;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner lectura = new Scanner (System.in);
+		
+		int lado1 = 0;
+		int lado2 = 0;
+		int lado3 = 0;
+		
+		
+		System.out.println("Ingrese Lado 1: ");
+		lado1 = lectura.nextInt();
+
+		System.out.println("Ingrese Lado 2: ");
+		lado2 = lectura.nextInt();
+		
+		System.out.println("Ingrese Lado 3: ");
+		lado3 = lectura.nextInt();
+		
+		Triangulo triangulo = new Triangulo(lado1, lado2, lado3);
+		
+		if( !triangulo.esTriangulo())
+		{
+			System.out.println("No es un triangulo");
+		}
+		else
+		{
+			System.out.println(triangulo.getTipo());	
+		}
+		
+		
+
+	}
+
+}
